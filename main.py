146d48a99594e07dbf14111e46096ad22dd49b9e
Kat Gao <kat.gao@koddi.com>
2023-08-15 11:35:27 -0500

    parse

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import json
+import time
+
 from datadog_api_client import ApiClient, Configuration
 from datadog_api_client.v2.api.logs_api import LogsApi
 from datadog_api_client.v2.model.logs_aggregate_request import LogsAggregateRequest
@@ -13,7 +16,9 @@ from datadog_api_client.v2.model.logs_sort_order import LogsSortOrder
 import re
 
 
-def get_datadog_logs(member_group_id, since, until):
+aggregated_counts = {}
+
+def get_body(member_group_id, since, until):
     body = LogsAggregateRequest(
         compute=[
             LogsCompute(
@@ -31,42 +36,73 @@ def get_datadog_logs(member_group_id, since, until):
         ),
         group_by=[
             LogsGroupBy(
-                facet="log_member_group",
+                facet="log_path",
                 sort=LogsAggregateSort(
-                    type=LogsAggregateSortType.MEASURE,
+                    # type=LogsAggregateSortType.MEASURE,
                     order=LogsSortOrder.DESCENDING,
-                    aggregation=LogsAggregationFunction.COUNT,
+                    # aggregation=LogsAggregationFunction.COUNT,
                 ),
-                limit=100,
+                limit=999,
             ),
+        ],
+    )
+
+    return body
+
+
+def get_body_with_page(member_group_id, since, until, page):
+    body = LogsAggregateRequest(
+        compute=[
+            LogsCompute(
+                aggregation=LogsAggregationFunction.COUNT,
+                metric="count",
+            ),
+        ],
+        filter=LogsQueryFilter(
+            _from=since,
+            indexes=[
+                  "*",
+            ],
+            query=f"service:emissary cluster_name:prod* @member_group:{member_group_id}",
+            to=until,
+        ),
+        group_by=[
             LogsGroupBy(
                 facet="log_path",
                 sort=LogsAggregateSort(
-                    type=LogsAggregateSortType.MEASURE,
+                    # type=LogsAggregateSortType.MEASURE,
                     order=LogsSortOrder.DESCENDING,
-                    aggregation=LogsAggregationFunction.COUNT,
+                    # aggregation=LogsAggregationFunction.COUNT,
                 ),
-                limit=100,
+                limit=999,
             ),
         ],
-        page=LogsAggregateRequestPage(),
+        page=LogsAggregateRequestPage(cursor=page),
     )
+
+    return body
+
+
+def get_datadog_logs(member_group_id, since, until, page=None):
+    body = None
+    if page:
+        body = get_body_with_page(member_group_id, since, until, page)
+    else:
+        body = get_body(member_group_id, since, until)
 
     configuration = Configuration()
 
     with ApiClient(configuration) as api_client:
         api_instance = LogsApi(api_client)
         response = api_instance.aggregate_logs(body=body)
-
-        print(response.meta, "data returned!!!")
-        return response.data.buckets
+        return response.data.buckets, response.meta
 
 
 def parse_data(data, member_group_id):
-    aggregated_counts = {}
 
     member_group_pattern = r"/\d+"
     draft_pattern = r"/drafts/[^/]+"
+    payment_pattern = r'/payment_methods/.*'
 
     for entry in data:
         log_path = entry["by"]["log_path"]
@@ -78,15 +114,45 @@ def parse_data(data, member_group_id):
         # Replace long string ID after 'drafts/' with '*'
         log_path = re.sub(draft_pattern, r"/drafts/*", log_path)
 
+        # Replace long string ID after 'payment_methods/' with '*'
+        log_path = re.sub(payment_pattern, r"/payment_methods/*", log_path)
+
         key = log_path
 
         aggregated_counts[key] = aggregated_counts.get(key, 0) + count
 
-    for log_path, count in aggregated_counts.items():
-        print(f"Log Member Group: 117, Modified Log Path: {log_path}, Count: {count}")
+
+def process_data(member_group_id, since, until, page=None, page_count=0):
+    buckets, meta = get_datadog_logs(member_group_id, since, until, page)
+    # catch the nil cursor
+    if "page" in meta:
+        page = meta.page.after
+    else:
+        page = None
+
+    print("Page: ", page[-10:])
+    bucket_length = len(buckets)
+    parse_data(buckets, member_group_id)
+    print(f"Page {page_count} processed")
+
+    print("bucket length: ", bucket_length)
+    page_count += 1
+
+    if page:
+        process_data(member_group_id, since, until, page, page_count)
 
 
 if __name__ == '__main__':
-    data = get_datadog_logs(117, "1672521600000", "1699366400000")
-    parse_data(data, 117)
+    print("starting processing")
 
+    process_data(117, "1689435406465", "1692027406465")
+
+    print("finished processing")
+
+    total = sum(aggregated_counts.values())
+
+    print(f"Total logs: {total}")
+
+    with open('output.json', 'w') as file:
+        # Serialize the dictionary to JSON and write to the file
+        json.dump(aggregated_counts, file, indent=4)  # The 'indent' parameter adds formatting for readability
